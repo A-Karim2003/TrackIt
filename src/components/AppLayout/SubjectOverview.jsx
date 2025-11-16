5e417024d96cc6d5b749e441cebd73d598ded5b7
Abdoulkarim <17a.karim@gmail.com>
2025-11-16 11:40:17 +0000

    feat(dashboard): add SubjectOverview and SubjectProgress components
    Adds subject summary section with reusable progress item, showing task counts
    and a visual progress bar. Sets foundation for dynamic data integration.

--- src/components/AppLayout/SubjectOverview.jsx
+++ src/components/AppLayout/SubjectOverview.jsx
@@ -1,9 +1,19 @@
+import SubjectProgress from "./SubjectProgress";
+
 function SubjectOverview() {
   return (
-    <section className="w-full m-auto p-8 flex gap-4 shadow-sm bg-white rounded-xl">
-      <div className="flex items-center justify-between w-full cursor-pointer">
+    <section className="w-full m-auto p-8 flex flex-col gap-4 shadow-sm bg-white rounded-xl">
+      <div className="flex items-center justify-between w-full">
         <h3 className="font-bold text-xl tracking-[1px]">Subject Overview</h3>
-        <small className="text-[#5853E7] text-[16px]">View All &rarr;</small>
+        <small className="text-[#5853E7] text-[16px] cursor-pointer">
+          View All &rarr;
+        </small>
+      </div>
+
+      <div className="flex flex-col gap-12">
+        <SubjectProgress subjectName={"React"} taskQuantity={2} />
+        <SubjectProgress subjectName={"JavaScript"} taskQuantity={4} />
+        <SubjectProgress subjectName={"Data Science"} taskQuantity={1} />
       </div>
     </section>
   );
---
+++ src/components/AppLayout/SubjectProgress.jsx
@@ -0,0 +1,19 @@
+function SubjectProgress({ subjectName, taskQuantity }) {
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center gap-4 j">
+        <div className="h-[15px] w-[15px] bg-customGreen rounded-full "></div>
+        <h4>{subjectName}</h4>
+        <small className="text-[14px] text-customGrey">
+          {taskQuantity} tasks
+        </small>
+        <small className="text-[14px] text-customGrey ml-auto">50%</small>
+      </div>
+      <div className="w-full h-2 bg-slate-200 mt-4 rounded-lg relative overflow-hidden">
+        <div className="absolute left-0 bg-customGreen h-full w-[50%]"></div>
+      </div>
+    </div>
+  );
+}
+
+export default SubjectProgress;
